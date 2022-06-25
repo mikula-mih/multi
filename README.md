9bc004480baff752adf3dc907192b766a63a4d8e
Mikula Mikhail <morpheus26@protonmail.com>
2022-06-25 17:39:33 +0300

    init UI + python multithreding

---
+++ README.md
@@ -0,0 +1,2 @@
+API server binds to `127.0.0.1:4067`. It means that you can only access your UI from localhost.
+
---
+++ multithread-image.py
@@ -0,0 +1,16 @@
+from PIL import Image
+import numpy as np
+import glob
+from keras.preprocessing.image import ImageDataGenerator
+from multiprocessing.dummy import Pool as ThreadPool
+
+
+def augment_image(raw_images, files, mult_factor):
+	pass
+
+def threaded_augment_images():
+	pass
+
+if __name__ == "__main__":
+	pass
+
